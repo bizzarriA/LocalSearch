26a9c61251354b561c9a8b29e928c25f0ab4b36a
tenaniSG <40560169+tenaniSG@users.noreply.github.com>
2019-07-25 12:03:27 +0200

    error_fixed

--- BestGreedy.pl
+++ BestGreedy.pl
@@ -50,7 +50,7 @@ controllo_grado_loop(N,[[Id,N1,N2,_]|ListaArchi], ArchiSpanning,K,C):-
 	((C==K1)
 		->false	/*se raggiungo K-1 sono già al limite ed è inutile controllare altri archi */
 	),
-	((Val\=1); (N\=N1, N\=N2)) 
+	((Val\=1; (N\=N1, N\=N2)) 
 		-> controllo_grado_loop(N,ListaArchi,ArchiSpanning,C) /*se l'arco non è selezionato oppure se il valore di N non corrisponde a nessun estremo dell'arco considerato vado oltre*/
 		; C is C1+1, controllo_grado_loop(N,ListaArchi,ArchiSpanning,C1) /*se invece l'arco è selezionato ed il nodo corrisponde ad almeno uno degli estremi incremento C */
 	).
