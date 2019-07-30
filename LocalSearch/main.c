694ba5f841f878d69000332a97cb40e2c8f0af14
tenaniSG <40560169+tenaniSG@users.noreply.github.com>
2019-07-30 12:33:46 +0200

    KHBFSHUFSBDFSFSDFS

--- LocalSearch/main.c
+++ LocalSearch/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, ALICEfHHHSARAjva!\n");
-    return 0;
+#define NUMERONODI 5
+#define NUMEROARCHI 8
+
+typedef struct s_arco{
+    int Id;
+    int N1;
+    int N2;
+    int Costo;
+    int Selected;
+}arco;
+
+arco individuaArco(arco ListaArchi[]){
+    int min_costo=100;
+    int min_id=0;
+    for(int i=0; i <NUMEROARCHI;i++){
+        if (ListaArchi[i].Costo<min_costo && ListaArchi[i].Selected==0){
+            min_costo=ListaArchi[i].Costo;
+            min_id=ListaArchi[i].Id;
+        }
+    }
+}
+
+void main() {
+    int Nodi[NUMERONODI];
+    arco ListaArchi[NUMEROARCHI];
+
+
+
+
 }
