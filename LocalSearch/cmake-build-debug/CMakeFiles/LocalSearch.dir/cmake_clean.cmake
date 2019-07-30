21ea537538cbbb405d3edc797935fcad9adbdf6f
bizzarriA <53293367+bizzarriA@users.noreply.github.com>
2019-07-30 11:42:28 +0200

    Prova

---
+++ LocalSearch/cmake-build-debug/CMakeFiles/LocalSearch.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LocalSearch.dir/main.c.obj"
+  "LocalSearch.pdb"
+  "LocalSearch.exe"
+  "LocalSearch.exe.manifest"
+  "LocalSearch.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LocalSearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
