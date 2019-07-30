acf86c1cb25f754a2995c9f0686fa5dba585cde6
bizzarriA <53293367+bizzarriA@users.noreply.github.com>
2019-07-30 11:22:21 +0200

    Create LS

---
+++ LS/cmake-build-debug/CMakeFiles/LS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LS.dir/main.c.obj"
+  "LS.pdb"
+  "LS.exe"
+  "LS.exe.manifest"
+  "LS.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
