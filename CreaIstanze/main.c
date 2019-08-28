8e0fe1ef746bbd076255f0583deed56a459c4497
bizzarriA <53293367+bizzarriA@users.noreply.github.com>
2019-08-28 17:08:47 +0200

    Non funziona comunque
    va solo in debug

--- CreaIstanze/main.c
+++ CreaIstanze/main.c
@@ -57,12 +57,13 @@ void greedyCostruttiva(arco* SoluzioneAttuale, int* NodiAttuali, int Numero_nodi
     int DaRicontrollare[NUMEROARCHI]={0}; //contiene gli Id di archi che non potevano essere presi al momento perchè non adiacenti ad altri
     while(!allNodes(NodiAttuali,Numero_nodi)) {
         srand ( time(NULL) );
-        i=rand() % Numero_archi-1;
+        i=rand() % (Numero_archi);
+        i=rand() % (Numero_archi);
         IdArco=IdArchi[i];
-        if(SoluzioneAttuale[IdArco-1].Selected!=1 && is_percorso(SoluzioneAttuale[IdArco-1],NodiAttuali) || gradoEccessivo(SoluzioneAttuale[IdArco-1],NodiAttuali,Grado_massimo)) {
+        if((SoluzioneAttuale[IdArco-1].Selected!=1 && is_percorso(SoluzioneAttuale[IdArco-1],NodiAttuali)) || gradoEccessivo(SoluzioneAttuale[IdArco-1],NodiAttuali,Grado_massimo)) {
             SoluzioneAttuale[IdArco-1].Ammissibile=0;
         }
-        else if(SoluzioneAttuale[IdArco-1].Selected!=1 && PrimoGiro || almenoUnNodo(SoluzioneAttuale[IdArco-1],NodiAttuali)){
+        else if((SoluzioneAttuale[IdArco-1].Selected!=1 && PrimoGiro) || almenoUnNodo(SoluzioneAttuale[IdArco-1],NodiAttuali)){
             PrimoGiro=0;
             SoluzioneAttuale[IdArco-1].Selected=1;
             NodiAttuali[SoluzioneAttuale[IdArco-1].N1-1]++;
@@ -90,7 +91,7 @@ void main() {
     int ListaId[NUMEROARCHI];
     int ListaNodi[NUMERONODI]={0};
 
-   /* printf("Per quale tipo di ricerca si vuole generare l'istanza?\nG: greedy\nL: local search\nR: grasp\nT: tabu\n");
+  /*printf("Per quale tipo di ricerca si vuole generare l'istanza?\nG: greedy\nL: local search\nR: grasp\nT: tabu\n");
     scanf("%c",&c);
     printf("Quanti nodi deve avere il grafo?\n");
     scanf("%d",&n);
@@ -102,7 +103,8 @@ void main() {
     FILE *fd;
 
     if(c=='G'){
-        fp=fopen("C:\\Users\\Sara\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.pl","w");
+        fp=fopen("C:\\Users\\Alice\\OneDrive\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.txt", "w");
+        //fp=fopen("C:\\Users\\Sara\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.pl","w");
         if(fp==NULL){
             printf("Errore apertura file");
             exit(1);
@@ -118,9 +120,10 @@ void main() {
         }
     }else if(c=='L'){
         n=5;
-        g=2;
+        g=3;
         //creo un file con archi, ma nessuno è preso
-        fp=fopen("C:\\Users\\Sara\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.txt","w");
+        fp=fopen("C:\\Users\\Alice\\OneDrive\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.txt", "w");
+        //fp=fopen("C:\\Users\\Sara\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.txt","w");
         if(fp==NULL){
             printf("Errore apertura file");
             exit(1);
@@ -132,13 +135,17 @@ void main() {
                 count++;
             }
         }
+        //chiudo file per salvare inserimento
+        fclose(fp);
         //apro il file per leggere gli archi e darli in pasto alla greedy che calcolerà la soluzione iniziale
-        fd=fopen("C:\\Users\\Sara\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.txt", "r");
+        //fd=fopen("C:\\Users\\Sara\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.txt", "r");
+        fd=fopen("C:\\Users\\Alice\\OneDrive\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.txt", "r");
         if(fd==NULL){
             printf("Errore apertura file");
             exit(1);
         }
         i=0;
+        scan=0;
         while(scan!=EOF){   //questa scanf legge solo gli archi perchè l'array con i gradi dei nodi se lo calcola la greedy
             scan=fscanf(fd,"%d/%d/%d/%d",&ListaArchi[i].Id,&ListaArchi[i].N1,&ListaArchi[i].N2,&ListaArchi[i].Costo);
             ListaArchi[i].Selected=0;
@@ -148,8 +155,8 @@ void main() {
         fclose(fd);
         individuaId(ListaArchi,ListaId);
         greedyCostruttiva(ListaArchi,ListaNodi,n,ListaId,count-1,g);
-
-        fp=fopen("C:\\Users\\Sara\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.txt","w");
+        fp=fopen("C:\\Users\\Alice\\OneDrive\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.txt", "w");
+        //fp=fopen("C:\\Users\\Sara\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.txt","w");
         if(fp==NULL){
             printf("Errore apertura file");
             exit(1);
@@ -160,7 +167,8 @@ void main() {
             fprintf(fp,"%d/%d/%d/%d/%d\n",ListaArchi[j].Id,ListaArchi[j].N1,ListaArchi[j].N2,ListaArchi[j].Costo,ListaArchi[j].Selected);
         fclose(fp);
     }else if(c=='R') {
-        fp=fopen("C:\\Users\\Sara\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.txt","w");
+        fp=fopen("C:\\Users\\Alice\\OneDrive\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.txt", "w");
+        //fp=fopen("C:\\Users\\Sara\\Documents\\GitHub\\LocalSearch\\CreaIstanze\\nuova_istanza.txt","w");
         if(fp==NULL){
             printf("Errore apertura file");
             exit(1);
