bce6c2161197aa00bbec2c49db10749c9f42c8b9
tenaniSG <40560169+tenaniSG@users.noreply.github.com>
2019-08-28 12:10:00 +0200

    non va

---
+++ CreaIstanze/cmake-build-debug/CMakeFiles/CreaIstanze.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CreaIstanze.dir/main.c.obj"
+  "CreaIstanze.pdb"
+  "CreaIstanze.exe"
+  "CreaIstanze.exe.manifest"
+  "libCreaIstanze.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CreaIstanze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
